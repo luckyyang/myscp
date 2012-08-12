25397dd6f90145158414496878ed2e67561e946e
Liu Jiyang <2411mail@gmail.com>
2012-08-12 10:12:39 +0800

    liujiyang: finally temp
    Signed-off-by: Liu Jiyang <2411mail@gmail.com>

--- test.py
+++ test.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python 
 
 """This command use to import date from 2012-01-01 to your current date into a file with format csv/excel.
-
 """
-
 import csv
 import sys
 import getopt
 import datetime
-from xlrd import open_workbook,cellname
 from tempfile import TemporaryFile
 from xlwt import Workbook
 
@@ -22,7 +19,7 @@ def main():
         print msg
         print "for help use [-h|--help]"
         sys.exit(2)
-    if sys.argv[1:] is None:
+    if sys.argv[1:] == []:
         print "are you kidding? please enter some params to let me to parse, this is why I am a program!"
         print "for help use [-h|--help]"
         sys.exit(2)
@@ -36,12 +33,11 @@ def main():
             sys.exit(0)
         elif o in ("-f", "--format"):
             outputFormat = a
-            if getattr(output_format, output_%s %outputFormat) is None:
+            if getattr(output_format, "output_%s" %outputFormat) is 'output_None':
                 print "can not deal with your format, will use csv format for default"
                 usage()
         elif o in ("-o", "--output"):
             outputFileName = a
-
     today = datetime.date.today()
     currentYear = today.year
     currentMonth = today.month
@@ -50,7 +46,6 @@ def main():
     firstDateThisYear = datetime.date(currentYear, 1, 1)
     tempDate = firstDateThisYear
 
-    #outputHead= ['#', 'Year', 'Month', 'Day']
     storeDateIntoList = ['#', 'Year', 'Month', 'Day']
 
     index = 1
@@ -58,61 +53,45 @@ def main():
     while (tempDate <= today):
         convertDateToRow = tempDate.isoformat().split('-')
         convertDateToRow.insert(0, str(index))
-        storeDateIntoList.append(convertDateToRow)
+        storeDateIntoList.extend(convertDateToRow)
         tempDate += datetime.timedelta(days=1)
         index += 1
 
-    output_function(outputFormat)
+    of = output_format()
+    output_function(of, storeDateIntoList, outputFileName, outputFormat)
 
-    def output_function(data, outputFileName, outputFormat="csv"):
-        output_function = getattr(output_format, output_%s %outputFormat, output_format.output_csv)
-        return output_function(data)
+def output_function(classInstance, data, outputFileName, outputFormat="csv"):
+    output_function = getattr(classInstance, "output_%s" %outputFormat, output_format.output_csv)
+    return output_function(data, outputFileName)
 
-class output_format(data):
-    def output_csv(data):
+class output_format:
+    def output_csv(self, data, outputFileName):
         csvFile = open(outputFileName, 'wb')
-
-        CSVFIELDS = ['#', 'Year', 'Month', 'Day']
-
         writer = csv.writer(csvFile)
 
-        writer.writerow(CSVFIELDS)
-
-        index = 1
-
+        index = 0
         try:
-            while (tempDate <= today):
-                storeDateIntoList = tempDate.isoformat().split('-')
-                toPrintDate = toListDate.insert(0, str(index))
-                tempDate += datetime.timedelta(days=1)
-                index += 1
-                writer.writerow(toListDate)
+            while (index <= len(data)):
+                writer.writerow(data[index:(index + 4)])
+                index += 4
         finally:
             csvFile.close()
 
-    def output_excel(data):
+    def output_excel(self, data, outputFileName):
         book = Workbook()
         sheet1 = book.add_sheet('Sheet 1')
 
-        sheet1.write(0,0,'A1')
-        sheet1.write(0,1,'B1')
-        row1 = sheet1.row(1)
-        row1.write(0,'A2')
-        row1.write(1,'B2')
-        sheet1.col(0).width = 3000
-        book.save('format.xls')
-        book.save(TemporaryFile())
-
-        book = open_workbook("format.xls")
-        sheet = book.sheet_by_index(0)
-        print sheet.name
-        print sheet.nrows
-        print sheet.ncols
-        for row_index in range(sheet.nrows):
-            for col_index in range(sheet.ncols):
-                print cellname(row_index,col_index),'-',
-                    print sheet.cell(row_index,col_index).value
-
+        index = 0
+        rowNumber = 0
+        try:
+            while (index < len(data)):
+                for i in range(4):
+                    sheet1.write(rowNumber, i, data[index+i])
+                index += 4
+                rowNumber += 1
+        finally:
+            book.save(outputFileName)
+            book.save(TemporaryFile())
 
 if __name__ == "__main__": 
     main()  
